438e1b0ec828b3db62fd77f9a3c6fff7fd472ca6
naren579 <147800440+naren579@users.noreply.github.com>
2024-05-31 16:30:05 +0530

    Update T5_text_summarization.py

--- Text Summarization/T5_text_summarization.py
+++ Text Summarization/T5_text_summarization.py
@@ -7,29 +7,33 @@ with st.sidebar:
 			st.success('Token submitted successfully!')
 API_URL = "https://api-inference.huggingface.co/models/Naren579/T5-Text-summarize"
 headers = {"Authorization": f"Bearer {API_TOKEN}"}
-
 def query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.json()
-	
-output = query({
-	"inputs": st.text_input('ENter your text here:'),
-})
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
 
-# st.write(output[0][0]['label'])
-if st.button('Summarize') and (API_TOKEN is not None):
-	st.write(output)
+def summarize_text(text):
+    payload = {
+        "inputs": text,
+        "parameters": {
+            "max_length": 512,  # Adjust as needed
+            "min_length": 50,   # Adjust as needed
+            "length_penalty": 2.0,
+            "num_beams": 4,
+        }
+    }
+    response = query(payload)
+#[0]['summary_text']
+    return response  # Adjust based on actual response structure
+    
 
-# 	if (output[0][0]['label']) == 'LABEL_0':
-# 		st.markdown("# The Sentence Seems to be POSITIVE")
-# 		st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1C4VPejYDvywKmk12MHyeH1z0ubr0E1A8lg&usqp=CAU')
-# 	elif (output[0][0]['label']) == 'LABEL_1':
-# 		st.markdown("# The Sentence Seems to be NEGATIVE")
-# 		st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbbuDRvaFBgko-Kox-TUykBQFIqGU7p5SWt5kFoKK1p9B_LQWlPbswDfiJH6RpEGfqQbY&usqp=CAU')
-# 	else:
-# 		st.markdown("# The Sentence Seems to be NEUTRAL")
-# 		st.image('https://assets-global.website-files.com/5bd07788d8a198cafc2d158a/61c49a62dccfe690ca3704be_Screen-Shot-2021-12-23-at-10.44.27-AM.jpg')
-# else:
-# 	st.error('Please provide your HuggingFace API key to continue.')
+# with st.sidebar:
+#     API_TOKEN = st.text_input('Enter your huggingface API key here')
+#     if st.button('Submit'):
+#         if API_TOKEN:
+#             st.success('Token submitted successfully!')
 
-
+text_input = st.text_input('Enter your text here:')
+text=text_input
+if st.button('Summarize') and API_TOKEN:
+    summarized_text = summarize_text(text)
+    st.write(summarized_text[0]['generated_text'])
